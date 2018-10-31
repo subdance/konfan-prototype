cb6f5f0b78d543c2dd6f45dd603b11a4884f4dcd
sub/dance <subdance@126.com>
2018-10-31 21:50:55 +0800

    background problem

--- src/components/character_show.vue
+++ src/components/character_show.vue
@@ -1,52 +1,54 @@
-<template>
-  <div id="showContainer">
-    <el-tabs tab-position="left">
-      <el-tab-pane label="平泽 唯">
-        <div class="characterHolder">
-
-        </div>
-      </el-tab-pane>
-      <el-tab-pane label="田井中 律">
-        <div class="characterHolder">
-
-        </div>
-      </el-tab-pane>
-      <el-tab-pane label="秋山 澪">
-        <div class="characterHolder">
-        </div>
-      </el-tab-pane>
-      <el-tab-pane label="琴吹 ">
-        <div class="characterHolder">
-
-        </div>
-      </el-tab-pane>
-      <el-tab-pane label="中野 梓">
-        <div class="characterHolder">
-
-        </div>
-      </el-tab-pane>
-    </el-tabs>
+<template lang="html">
+  <div class="">
+    <div class="tabHolder" :style="{ backgroundImage: 'url(' + nowBackPic + ')' }">
+      <div class="tabHeaderHolder">
+        <ul>
+          <li @click='showCharacter(1)'>平泽 唯</li>
+          <li @click='showCharacter(2)'>田井中 律</li>
+          <li @click='showCharacter(3)'>秋山 澪</li>
+          <li @click='showCharacter(4)'>琴吹</li>
+          <li @click='showCharacter(5)'>中野 梓</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
+
 <script>
-  export default {
-    data() {
-      return {
-      };
+export default {
+  data(){
+    return{
+      nowBackPic: require('../assets/home_pic/1.jpg'),
+      backPic: ['../assets/home_pic/1.jpg',]
+    }
+  },
+  methods: {
+    showCharacter(index){
+
     },
-    methods: {
-    },
-  };
+  },
+}
 </script>
+
 <style scoped>
-  #showContainer {
-    margin-top: 62px;
-    background-image: url('../assets/home_pic/1.jpg');
+  * {
+    box-sizing: border-box;
+  }
+  ul, li {
+    list-style-type: none;
+    padding: 0px;
+    margin: 0px;
+  }
+  .tabHolder {
     background-position: center;
     background-size: cover;
-  }
-  .characterHolder {
     width: 100%;
     height: 600px;
+    border: 2px solid red;
+  }
+  .tabHeaderHolder {
+    margin-top: 120px;
+    border: 2px solid red;
+    display: inline-block;
   }
 </style>
