5dde3b650515aae73be4d2efc789d99cf3a18434
sub/dance <subdance@126.com>
2018-11-04 17:10:29 +0800

    add new ::after

--- src/components/character_intro.vue
+++ src/components/character_intro.vue
@@ -47,7 +47,20 @@ export default {
    color: #909399;
  }
  .introHolder {
+   /* background-image: url('../assets/background/4.jpg'); */
+   position: relative;
+   z-index: 1;
+ }
+ .introHolder::after {
    background-image: url('../assets/background/4.jpg');
+   opacity: 0.6;
+   content: '';
+   position: absolute;
+   top: 0;
+   left: 0;
+   bottom: 0;
+   right: 0;
+   z-index: -1;
  }
  .imgHolder {
    text-align: center;
