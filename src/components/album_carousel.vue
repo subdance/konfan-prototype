d0345d8a133761a55c537b45fcb8d675e0f26180
sub/dance <subdance@126.com>
2018-11-18 22:25:15 +0800

    ryme up!

--- src/components/album_carousel.vue
+++ src/components/album_carousel.vue
@@ -2,7 +2,10 @@
   <div class="mainHolder">
     <div class="titleHolder">
       <div class="textHolder">
-        <div class="icon">
+        <!-- <div class="icon">
+
+        </div> -->
+        <div class="text">
           <span class="iconStyle">
             <i class="fa fa-camera-retro fa-5x"></i>
             &nbsp &nbsp
@@ -12,27 +15,63 @@
             &nbsp &nbsp
             <i class="fa fa-music fa-5x"></i>
           </span>
-        </div>
-        <div class="text">
           <span class="titileText">捕捉轻音部的闪光瞬间(●'◡'●)</span>
         </div>
-
       </div>
     </div>
-    <el-carousel
-      indicator-position="outside"
-      height='40rem'
-      :interval="4000"
-      type="card"
-      trigger='click'
-      >
-      <el-carousel-item
-        v-for="item in 5"
-        :key="item"
+
+    <div class="carouselPart">
+      <p class="introText">唯律澪紬梓的乐器</p>
+      <el-carousel
+        indicator-position="outside"
+        height='40rem'
+        :interval="4000"
+        type="card"
+        trigger='click'
         >
-        <img class="carouselPic" :src='nowPic[item - 1]' alt="">
-      </el-carousel-item>
-    </el-carousel>
+        <el-carousel-item
+          v-for="item in 5"
+          :key="item"
+          >
+          <img class="carouselPicCard" :src='nowPic1[item - 1]' alt="">
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+
+    <div class="carouselPart">
+      <p class="introText">烟火下看到梦想的样子</p>
+      <el-carousel
+        indicator-position="outside"
+        height='60rem'
+        :interval="4000"
+        trigger='click'
+        >
+        <el-carousel-item
+          v-for="item in 6"
+          :key="item"
+          >
+          <img class="carouselPic" :src='nowPic2[item - 1]' alt="">
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+
+    <div class="carouselPart">
+      <p class="introText">律澪</p>
+      <el-carousel
+        indicator-position="outside"
+        height='60rem'
+        :interval="4000"
+        trigger='click'
+        >
+        <el-carousel-item
+          v-for="item in 5"
+          :key="item"
+          >
+          <img class="carouselPic" :src='nowPic3[item - 1]' alt="">
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+
   </div>
 
 </template>
@@ -41,12 +80,27 @@
 export default {
   data(){
     return {
-      nowPic: [
-        require('../assets/character_element/yuiI2.png'),
-        require('../assets/character_element/ritsuI2.png'),
-        require('../assets/character_element/mioI2.png'),
-        require('../assets/character_element/mugiI2.png'),
-        require('../assets/character_element/azusaI2.png'),
+      nowPic1: [
+        require('../assets/album/set1/1.png'),
+        require('../assets/album/set1/2.png'),
+        require('../assets/album/set1/3.png'),
+        require('../assets/album/set1/4.png'),
+        require('../assets/album/set1/5.png'),
+      ],
+      nowPic2: [
+        require('../assets/album/set2/1.png'),
+        require('../assets/album/set2/2.png'),
+        require('../assets/album/set2/3.png'),
+        require('../assets/album/set2/4.png'),
+        require('../assets/album/set2/5.png'),
+        require('../assets/album/set2/6.png'),
+      ],
+      nowPic3: [
+        require('../assets/album/set3/1.png'),
+        require('../assets/album/set3/2.png'),
+        require('../assets/album/set3/3.png'),
+        require('../assets/album/set3/4.png'),
+        require('../assets/album/set3/5.png'),
       ],
     }
   },
@@ -55,16 +109,21 @@ export default {
 
 <style scoped>
   .mainHolder {
-    height: 70rem;
+
   }
-  .carouselPic {
+  .carouselPicCard {
     border-radius: 5px;
     width: 100%;
     height: 40rem;
     object-fit: cover;
   }
+  .carouselPic {
+    width: 100%;
+    height: 60rem;
+    object-fit: cover;
+  }
   .titleHolder {
-    height: 20rem;
+    height: 30rem;
   }
   .textHolder {
     padding-top: 110px;
@@ -80,12 +139,19 @@ export default {
     padding-left: 3rem;
   }
   .iconStyle {
-    color: white;
     text-shadow: 2px 2px 2px grey;
   }
   .text {
     float: right;
     display: inline-block;
     text-align: right;
+  }
+  .introText {
+    color: #303133;
+    font-size: 2rem;
+    text-align: center;
+  }
+  .carouselPart {
+    margin-bottom: 10rem;
   }
 </style>
