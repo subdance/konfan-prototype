0731bacd18c63471f211ab40f60ea13a8982cf8e
sub/dance <subdance@126.com>
2018-11-22 21:25:57 +0800

    new collapse

--- src/components/teaRoom_article.vue
+++ src/components/teaRoom_article.vue
@@ -3,22 +3,36 @@
     <div class="mainHolder">
       <p>相信的<i class="fa fa-heart-o"></i>就是你的魔法</p>
     </div>
-    <div class="collapseHolder">
-      <button class="accordion" @click='changeShow(1)'>Section 1</button>
-      <div :class="{ active: isActive[0] ,panel: isDefault[0]}">
-        <p>Lorem ipsum...</p>
-      </div>
+    <div class="single_row">
+      <div class="collapse_title" @click='changeShow(0)'>
+        <span>this is title</span>
 
-      <button class="accordion" @click='changeShow(2)'>Section 2</button>
-      <div :class="{ active: isActive[1] ,panel: isDefault[1]}">
-        <p>Lorem ipsum...</p>
       </div>
-
-      <button class="accordion" @click='changeShow(3)'>Section 3</button>
-      <div :class="{ active: isActive[2] ,panel: isDefault[2]}">
-        <p>Lorem ipsum...</p>
+      <div class="collapse_text" :style="{maxHeight: maxHeight[0] + 'px'}">
+        <span>this is text</span>
+        <p>aa</p>
+        <p>aa</p>
       </div>
     </div>
+
+    <div class="single_row">
+      <div class="collapse_title" @click='changeShow(1)'>
+        <span>this is title</span>
+      </div>
+      <div class="collapse_text" :style="{maxHeight: maxHeight[1] + 'px'}">
+        <span>this is text</span>
+      </div>
+    </div>
+
+    <div class="single_row">
+      <div class="collapse_title" @click='changeShow(2)'>
+        <span>this is title</span>
+      </div>
+      <div class="collapse_text" :style="{maxHeight: maxHeight[2] + 'px'}">
+        <span>this is text</span>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -26,28 +40,25 @@
 export default {
   data(){
     return {
-      isActive: [false, false, false],
-      isDefault: [true, true, true,],
+      maxHeight: [0, 0, 0,],
     }
   },
   methods: {
     changeShow(index){
-      if(this.isActive[index - 1]){
-        this.$set(this.isActive, index - 1, false);
-        this.$set(this.isDefault, index - 1, true);
+      let nowText = document.getElementsByClassName('collapse_text')[index];
+      console.log(nowText.scrollHeight);
+      if(!this.maxHeight[index]){
+        for(let i = 0; i < this.maxHeight.length; i ++){
+          this.$set(this.maxHeight, i, 0);
+        }
+        this.$set(this.maxHeight, index, nowText.scrollHeight);
       }
       else{
-        for(let i = 0; i < this.isActive.length; i ++){
-          this.$set(this.isActive, i, false);
-          this.$set(this.isDefault, i, true);
-        }
-        if(!this.isActive[index - 1]){
-          this.$set(this.isActive, index - 1, true);
-          this.$set(this.isDefault, index - 1, false);
-        }
+        this.$set(this.maxHeight, index, 0);
       }
     },
   },
+
 }
 </script>
 
@@ -57,31 +68,23 @@ export default {
     font-size: 3rem;
     text-align: center;
   }
-  .accordion {
-    background-color: #eee;
-    color: #444;
+  * {
+    box-sizing: border-box;
+  }
+  .collapse_title {
+    height: 4rem;
+    font-size: 3rem;
+  }
+  .collapse_title:hover {
     cursor: pointer;
-    padding: 18px;
-    width: 100%;
-    text-align: left;
-    border: none;
-    outline: none;
-    transition: 0.4s;
   }
-  .active, .accordion:hover {
-    background-color: #ccc;
-  }
-  .panel {
-    padding: 0 18px;
-    background-color: white;
-    max-height: 0;
+  .collapse_text {
     overflow: hidden;
-    transition: max-height 0.2s ease-out;
+    transition: max-height 0.3s ease-out;
+    color: #606266;
   }
-  .active {
-    padding: 0 18px;
-    background-color: white;
-    overflow: hidden;
-    transition: max-height 0.2s ease-out;
+  .single_row {
+    border-bottom: 2px solid #F56C6C;
+    color: #303133;
   }
 </style>
