385186f369242f5596a4ec39c14a722e34736d23
sub/dance <subdance@126.com>
2018-12-31 18:55:13 +0800

    little improvment

--- src/components/teaRoom_sentence.vue
+++ src/components/teaRoom_sentence.vue
@@ -3,7 +3,6 @@
     <div class="sentenceContainer" v-for='(singleSentence) in sentence'>
       <div class="wordHolder" v-for='(word) in singleSentence.content'>
         <span class="word">{{ word }}</span>
-        <link async href="https://fonts.googleapis.com/css?family=ZCOOL+XiaoWei" rel="stylesheet">
       </div>
       <div class="sourceHolder" v-for='(word) in singleSentence.source'>
         <span class="source">{{ word }}</span>
@@ -31,6 +30,10 @@ export default {
           content: '我的刻薄话使用铜四十克、锌二十五克、镍十五克、害羞五克、再加上恶意九十七公斤配成的。顺便说一下，害羞是骗人的。',
           source: '化物语'
         },
+        {
+          content: '能让封闭内心的人随心所欲地说出想说的话的对象只有两种。一种是被对方讨厌了也没关系的，还有一种是不必担心被对方讨厌。',
+          source: '化物语'
+        }
       ],
       sentence: [],
     }
@@ -57,13 +60,14 @@ export default {
     max-height: 600px;
     width: 100px;
     flex-wrap: wrap-reverse;
-    align-items: flex-start;
-    margin-right: 10px;
+    align-content: center;
+    /* margin-right: 10px; */
   }
   .wordHolder {
     text-align: center;
     border: 1px solid black;
     transition: 0.3s;
+    margin-left: 3px;
   }
   .word {
     font-size: 2rem;
@@ -71,9 +75,9 @@ export default {
   }
   .sourceHolder {
     text-align: center;
-    /* align-self: stretch; */
     border: 1px solid black;
     transition: 0.3s;
+    margin-left: 3px;
   }
   .source {
     font-size: 2rem;
@@ -83,11 +87,12 @@ export default {
   .wordHolder:hover {
     background-color: white;
     transform: scale(1.6, 1.6);
-    transition: 0.3s;
+    transition: 0.15s;
+    border: 1px solid white;
   }
   .sourceHolder:hover {
     background-color: black;
     transform: scale(1.6, 1.6);
-    transition: 0.3s;
+    transition: 0.15s;
   }
 </style>
