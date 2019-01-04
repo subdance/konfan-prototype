d7c7fd00fa940112462b240a8f9be22f081b37ed
Fhadiel <fhadiel.fhaaaa@gmail.com>
2019-01-04 10:26:25 +0700

    refractoring code

--- src/components/homePage_interview.vue
+++ src/components/homePage_interview.vue
@@ -1,31 +1,44 @@
 <template lang="html">
   <div class="mainContainer">
-    <div class="contentHolder">
-      <img class="contentImg" src="../assets/character/nodoka.jpg" alt="">
+    <div class="contentHolder" v-for="(content,i) in contents" :key="i" >
+      <img class="contentImg" :src="getImageUrl(content.pic)">
       <p class="contentText">
-        “轻音部啊...虽然平日里马马虎虎，但在学院祭上的表演十分有感染力。”</br>
-        <span class="name">学生会长--真锅和</br></span>
+        “{{content.text}}”</br>
+        <span class="name"> {{content.name}}</br></span>
       </p>
-    </div>
-    <div class="contentHolder">
-      <p class="contentText">
-        “姐姐她以前可以无聊到在地上打滚，自从开始学吉他之后，就很少看到她百无聊奈的样子了！姐姐她在轻音部真的成长了很多!”</br>
-        <span class="name">平泽唯的妹妹--平泽忧</br></span>
-      </p>
-      <img class="contentImg" src="../assets/character/ui.jpg" alt="">
-    </div>
-    <div class="contentHolder">
-      <img class="contentImg" src="../assets/character/sawako.jpg" alt="">
-        <p class="contentText">
-          “我十分感谢这群孩子让我当她们的顾问。还有很多特别的衣服想给她们穿呢。”</br>
-          <span class="name">班主任，轻音部顾问, 前轻音部成员--山中佐和子</br></span>
-        </p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      contents: [
+        {
+          pic: "../assets/character/nodoka.jpg",
+          text: "轻音部啊...虽然平日里马马虎虎，但在学院祭上的表演十分有感染力。",
+          name: "学生会长--真锅和",
+        },
+        {
+          pic: "../assets/character/ui.jpg",
+          text: "姐姐她以前可以无聊到在地上打滚，自从开始学吉他之后，就很少看到她百无聊奈的样子了！姐姐她在轻音部真的成长了很多!",
+          name: "平泽唯的妹妹--平泽忧"
+        },
+        {
+          pic: "../assets/character/sawako.jpg",
+          text: "我十分感谢这群孩子让我当她们的顾问。还有很多特别的衣服想给她们穿呢。",
+          name: "班主任，轻音部顾问, 前轻音部成员--山中佐和子"
+        }
+      ]
+    }
+  },
+  methods: {
+    getImageUrl(pic) {
+      console.log(pic)
+      return require(pic)
+    }
+  }
 }
 </script>
 
