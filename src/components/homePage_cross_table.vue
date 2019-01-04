dc4fbf213c9e287712054789cd53895e2a2b2e85
Fhadiel <fhadiel.fhaaaa@gmail.com>
2019-01-04 08:49:32 +0700

    refractoring code a little bit

--- src/components/homePage_cross_table.vue
+++ src/components/homePage_cross_table.vue
@@ -1,87 +1,24 @@
 <template lang="html">
   <div class="mainContainer">
     <div>
-      <div class="flexContainer">
+      <div class="flexContainer" v-for="(intro,index) in intros">
         <div
           class="flexPicItem"
-          :style="{ backgroundImage: 'url(' + introPic[0] + ')' }"
-          @mouseenter='setDisplay(0)'
-          @mouseleave='setHide(0)'
+          :style="{ backgroundImage: `url(${intro.pic})`,order: index%2==0? 0:1 }"
+          @mouseenter="setDisplay(index)"
+          @mouseleave="setHide(index)"
           >
           <div
             class="innerHolder"
-            v-show='innerDisplayArray[0]'
-            >
-            于2009年播出的轻音少女，成为了大热的现象级作品，甚至有了强国之源的美誉。
-          </div>
-        </div>
-        <div class="flexTextItem">
-          <div style="text-align: left;">
-            <p>{{ introTitle[0] }}</p>
-            <p>{{ introText[0] }}</p>
-          </div>
-        </div>
-      </div>
-      <div class="flexContainer">
-        <div class="flexTextItem">
-          <div style="text-align: right;">
-            <p>{{ introTitle[1] }}</p>
-            <p>{{ introText[1] }}</p>
-          </div>
-        </div>
-        <div
-          class="flexPicItem"
-          :style="{ backgroundImage: 'url(' + introPic[1] + ')' }"
-          @mouseenter='setDisplay(1)'
-          @mouseleave='setHide(1)'
+            v-text="intro.something"
+            v-show="intro.display"
           >
-          <div
-            class="innerHolder"
-            v-show='innerDisplayArray[1]'
-            >
-            校园，废萌，社团，音乐等在当时新鲜无比的动画元素让轻音迅速走红。
-          </div>
-        </div>
-      </div>
-      <div class="flexContainer">
-        <div
-          class="flexPicItem"
-          :style="{ backgroundImage: 'url(' + introPic[2] + ')' }"
-          @mouseenter='setDisplay(2)'
-          @mouseleave='setHide(2)'
-          >
-          <div
-            class="innerHolder"
-            v-show='innerDisplayArray[2]'
-            >
-            五位主角的声优成为了行业内的新星，日后多次在名作中出演主役。
           </div>
         </div>
         <div class="flexTextItem">
-          <div style="text-align: left;">
-            <p>{{ introTitle[2] }}</p>
-            <p>{{ introText[2] }}</p>
-          </div>
-        </div>
-      </div>
-      <div class="flexContainer">
-        <div class="flexTextItem">
-          <div style="text-align: right;">
-            <p>{{ introTitle[3] }}</p>
-            <p>{{ introText[3] }}</p>
-          </div>
-        </div>
-        <div
-          class="flexPicItem"
-          :style="{ backgroundImage: 'url(' + introPic[3] + ')' }"
-          @mouseenter='setDisplay(3)'
-          @mouseleave='setHide(3)'
-          >
-          <div
-            class="innerHolder"
-            v-show='innerDisplayArray[3]'
-            >
-            轻音的漫画依然在连载着，也许某一天这段温暖的故事会再次回归。
+          <div :style="{textAlign: index%2 == 0 ? 'left':'right'}">
+            <p>{{ intro.title }}</p>
+            <p>{{ intro.text }}</p>
           </div>
         </div>
       </div>
@@ -98,33 +35,44 @@ export default {
   },
   data(){
     return {
-      innerDisplayArray: [false, false, false, false, ],
-      introTitle: [
-        'K-ON是“轻音部”这一高中社团的简称',
-        '两位吉他手，一个键盘手，一个鼓手',
-        '可口的点心，悠闲的午后',
-        '生活，音乐，懵懂的梦想',
-      ],
-      introText: [
-        '在这一社团中，五位少女成立了名为“放学后下午茶”（HTT）的乐队',
-        '和一位贪吃的顾问，便是这个社团的全部成员',
-        '以及些许手忙脚乱的练习，构成了云朵般的日常',
-        '还有最最,最重要的——互相陪伴的彼此',
-      ],
-      introPic: [
-        'http://35.244.189.64/k-on-fan/image/home-pic/9.jpg',
-        'http://35.244.189.64/k-on-fan/image/home-pic/4.jpg',
-        'http://35.244.189.64/k-on-fan/image/home-pic/8.jpg',
-        'http://35.244.189.64/k-on-fan/image/home-pic/1.jpg',
-      ],
+      intros: [
+        {
+          title: 'K-ON是“轻音部”这一高中社团的简称',
+          text: '在这一社团中，五位少女成立了名为“放学后下午茶”（HTT）的乐队',
+          pic: 'http://35.244.189.64/k-on-fan/image/home-pic/9.jpg',
+          something: '于2009年播出的轻音少女，成为了大热的现象级作品，甚至有了强国之源的美誉。',
+          display: false,
+        },
+        {
+          title: '两位吉他手，一个键盘手，一个鼓手',
+          text: '和一位贪吃的顾问，便是这个社团的全部成员',
+          pic: 'http://35.244.189.64/k-on-fan/image/home-pic/4.jpg',
+          something: '校园，废萌，社团，音乐等在当时新鲜无比的动画元素让轻音迅速走红。',
+          display: false,
+        },
+        {
+          title: '可口的点心，悠闲的午后',
+          text: '以及些许手忙脚乱的练习，构成了云朵般的日常',
+          pic: 'http://35.244.189.64/k-on-fan/image/home-pic/8.jpg',
+          something: '五位主角的声优成为了行业内的新星，日后多次在名作中出演主役。',
+          display: false,
+        },
+        {
+          title: '生活，音乐，懵懂的梦想',
+          text: '还有最最,最重要的——互相陪伴的彼此',
+          pic: 'http://35.244.189.64/k-on-fan/image/home-pic/1.jpg',
+          something: '轻音的漫画依然在连载着，也许某一天这段温暖的故事会再次回归。',
+          display: false
+        }
+      ]
     }
   },
   methods: {
     setDisplay(index){
-      this.$set(this.innerDisplayArray, index, true);
+      this.intros[index].display = true
     },
     setHide(index){
-      this.$set(this.innerDisplayArray, index, false);
+      this.intros[index].display = false
     },
   },
 }
