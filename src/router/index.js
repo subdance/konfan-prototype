f277c0715904f8107e07aa1abb09fc71aed649fc
subdance <subdance@126.com>
2019-05-01 02:23:46 +0800

    调整文件结构～

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import homePage from '@/homepage.vue'
-import character from '@/character.vue'
-import album from '@/album.vue'
-import teaRoom from '@/tea_room.vue'
-import videoRoom from '@/video_room.vue'
+import homePage from '@/page_loader/homepage.vue'
+import character from '@/page_loader/character.vue'
+import album from '@/page_loader/album.vue'
+import teaRoom from '@/page_loader/tea_room.vue'
+import videoRoom from '@/page_loader/video_room.vue'
 
 Vue.use(Router)
 
