e5f71ba617b8260b2819dbe58e5a19831fed114c
subdance <subdance@126.com>
2019-09-08 03:02:34 +0800

    优化 调整组件引用方式

--- src/page_loader/homepage.vue
+++ src/page_loader/homepage.vue
@@ -1,13 +1,14 @@
 <template lang="html">
-  <div class="mainContainer">
+    <div class="mainContainer">
     <theater></theater>
     <div class="backgroundHolder">
-      <crossTable style="z-index: 20;"></crossTable>
-      <div class="bottomHolder">
+        <crossTable></crossTable>
+        <interview></interview>
+        <div class="bottomHolder">
         <img class="bottomImg" src="https://konfan.oss-cn-beijing.aliyuncs.com/image/home/10.png" alt="">
-      </div>
+        </div>
     </div>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -16,48 +17,23 @@ import interview from '@/components/homepage/homePage_interview.vue'
 import theater from '@/components/homepage/homePage_theater.vue'
 import crossTable from '@/components/homepage/homePage_cross_table.vue'
 export default {
-  components: {
-    interview,
-    theater,
-    crossTable,
-  },
-  data(){
-    return {
-      innerDisplayArray: [false, false, false, false, ],
-      introTitle: [
-        'K-ON是“轻音部”这一高中社团的简称',
-        '两位吉他手，一个键盘手，一个鼓手',
-        '可口的点心，悠闲的午后',
-        '生活，音乐，还有懵懂的梦想',
-      ],
-      introText: [
-        '在这一社团中，五位少女成立了名为“放学后下午茶”（HTT）的乐队',
-        '和一位贪吃的顾问，便是这个社团的全部成员',
-        '以及些许手忙脚乱的练习，构成了云朵般的日常',
-        '还有最最,最重要的——互相陪伴的彼此',
-      ],
-      introPic: [
-        'https://konfan.oss-cn-beijing.aliyuncs.com/image/home/9.jpg',
-        'https://konfan.oss-cn-beijing.aliyuncs.com/image/home/4.jpg',
-        'https://konfan.oss-cn-beijing.aliyuncs.com/image/home/8.jpg',
-        'https://konfan.oss-cn-beijing.aliyuncs.com/image/home/1.jpg',
-      ],
-    }
-  },
-  created(){
-    eventBus.$emit('changeFixPlayer', true);
-    eventBus.$emit('stopMusic', false);
-  },
-  computed:{
-  },
-  methods: {
-    setDisplay(index){
-      this.$set(this.innerDisplayArray, index, true);
+    components: {
+        interview,
+        theater,
+        crossTable,
     },
-    setHide(index){
-      this.$set(this.innerDisplayArray, index, false);
+    created(){
+        eventBus.$emit('changeFixPlayer', true);
+        eventBus.$emit('stopMusic', false);
     },
-  },
+    methods: {
+        setDisplay(index){
+            this.$set(this.innerDisplayArray, index, true);
+        },
+        setHide(index){
+            this.$set(this.innerDisplayArray, index, false);
+        },
+    },
 }
 </script>
 
