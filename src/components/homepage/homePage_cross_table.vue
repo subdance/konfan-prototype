c6ef5016da2e3768b19b5d22ed446b9cdb99ac1d
subdance <subdance@126.com>
2019-10-10 17:58:32 +0800

    hot fix

--- src/components/homepage/homePage_cross_table.vue
+++ src/components/homepage/homePage_cross_table.vue
@@ -61,14 +61,15 @@ export default {
         box-sizing: border-box;
     }
     .main-container {
-        width: 100%;
+        margin: auto;
+        max-width: 1450px;
         padding-top: 30px;
     }
     .single-line {
         width: 100%;
         display: flex;
         flex-flow: row nowrap;
-        justify-content: center;
+        justify-content: space-around;
         align-items: center;
     }
     .single-line:nth-child(2n + 1) {
@@ -144,13 +145,13 @@ export default {
         flex: 0 0 600px;
     }
     .text-holder > p {
-        text-align: left;
+        text-align: right;
         font-size: 2.2rem;
         text-shadow: 0 -0.05em 0.1em rgba(0,0,0,.3);
         font-family: 'ZCOOL KuaiLe', cursive;
     }
     .text-holder-odd > p {
-        text-align: right;
+        text-align: left;
     }
 
 
